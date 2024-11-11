fb59e71457e5569aa758d2ccf73751b76ffef269
kumarsandeep567 <kumar.sandee@northeastern.edu>
2024-11-11 00:44:15 -0500

    Minor UI changes

---
+++ frontend/src/app/api/copilotkit/route.ts
@@ -0,0 +1,29 @@
+import {
+  CopilotRuntime,
+  OpenAIAdapter,
+  copilotRuntimeNextJSAppRouterEndpoint,
+} from "@copilotkit/runtime";
+import OpenAI from "openai";
+import { NextRequest } from "next/server";
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const llmAdapter = new OpenAIAdapter({ openai });
+
+const runtime = new CopilotRuntime({
+  remoteActions: [
+    {
+      url: process.env.REMOTE_ACTION_URL || "http://localhost:8000/copilotkit",
+      // url: "https://sm2vrgnlj5aq57hbcd3xjlg2c40grcwk.lambda-url.us-east-1.on.aws/copilotkit"
+    },
+  ],
+});
+
+export const POST = async (req: NextRequest) => {
+  const { handleRequest } = copilotRuntimeNextJSAppRouterEndpoint({
+    runtime,
+    serviceAdapter: llmAdapter,
+    endpoint: "/api/copilotkit",
+  });
+
+  return handleRequest(req);
+};
